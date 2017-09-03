9f6d966eb1b5ab3ffa49809c094f37c48c5a130c
jenny0927 <jennychen927@gmail.com>
2017-09-03 12:50:17 -0400

    add more context

--- homework/homework_set1/futureValue.cpp
+++ homework/homework_set1/futureValue.cpp
@@ -1,16 +1,26 @@
 #include <iostream> 
+#include <cmath>
+#include<cstdio>
 using namespace std; 
 
 double future_value(double F0, double t0, double t1, double r);
 
 int main(){
-	fuure_value(100,1,180,5);
+	double cashValue = 100;
+	double interestRate = 5.0;
+	int todayTime = 1;
+	int futureTime = 2; 
+	int day = futureTime - todayTime;
+	cout << "Today's cash value is $" << cashValue << endl;
+	cout << day << " day(s) later, ";
+	cout << "the future value is $"<< future_value(cashValue,todayTime,futureTime,interestRate);
+	return 0;
 }
 
 
 double future_value(double F0, double t0, double t1, double r)
 {
-double r_decimal = 0.01*r;
-double F1 = F0*exp(r_decimal*(t1-t0));
-return F1;
+	double r_decimal = 0.01*r;
+	double F1 = F0*exp(r_decimal*(t1-t0));
+	return F1;
 }
